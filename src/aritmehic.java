9c838174a1f868c74bf03c7c6c98e23b8f46cc14
Romualds Leitans <Romualdlei@gmail.com>
2022-11-13 12:29:53 +0000

    arit

---
+++ src/aritmehic.java
@@ -0,0 +1,34 @@
+public class aritmehic {
+    public static void main (String [] args) {
+        int a = 10;
+        int b = 30;
+
+        int result = a + b;
+        int result1 = a - b;
+        int result2 = a / b;
+        int result3 = a * b;
+        int result4 = 20 % 8;
+        int result5 = 20 % 2; //0
+        int result6 = 21 % 2; //1
+
+
+        double x = 5.3;
+        double z = 4.5;
+        double resultDouble =  x + z;
+        double resultDouble2 = x - z;
+        double resultDouble3 = x * z;
+        double resultDouble4 = x / z;
+
+        int g = 10;
+        int h = 4;
+        double result55 = g /(double) h;
+        System.out.println(result55);
+
+
+        String hi = "hi";
+        System.out.println(hi);
+        System.out.print(g);
+        System.out.println("I write my first code");
+        }
+    }
+
